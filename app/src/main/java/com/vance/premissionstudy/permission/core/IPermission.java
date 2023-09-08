a599e1e5366c73fdc5842c3b7d1d0a99454215cf
vance <738297567@qq.com>
2023-09-08 18:18:17 +0800

    Initial commit

---
+++ app/src/main/java/com/vance/premissionstudy/permission/core/IPermission.java
@@ -0,0 +1,11 @@
+package com.vance.premissionstudy.permission.core;
+
+// 告诉外界
+public interface IPermission {
+
+    void ganted(); // 已经授权
+
+    void cancel(); // 取消权限
+
+    void denied(); // 拒绝权限
+}
